660d0b52099dd9977527e90753ae59d6a5bb8171
Denis Kibet <kibet1816@gmail.com>
2019-07-05 12:33:21 +0300

    Create authentication blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 # Import extensions
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 
 # Create extension instances
 app = Flask(__name__)
-db = SQLAlchemy()
 bootstrap = Bootstrap()
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 
 
 def create_app(config_name):
