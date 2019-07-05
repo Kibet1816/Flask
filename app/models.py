26f510f2feb0a67af485a3841458e7a9e1c4a1a7
Denis Kibet <kibet1816@gmail.com>
2019-07-05 11:29:25 +0300

    Initialized app to use migrations

--- app/models.py
+++ app/models.py
@@ -1,1 +1,16 @@
 from . import db
+
+
+class User(db.Model):
+    """
+    The user model class
+
+    Args:
+        DbModel:Connects our class to our database and allow communication
+    """
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(300))
+    email = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
